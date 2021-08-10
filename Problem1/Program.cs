fadd39a2d541ee4a7fa229e3a935efec08058398
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-10 22:11:05 +0500

    +problem1

--- Problem1/Program.cs
+++ Problem1/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
 
-namespace Problem1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Problem1 {
+    class Program {
+        static void Main(string[] args) {
+            
+            double price = Read("price");
+
+            if(price > 1000) 
+                price *= 0.95;
+            else if (price > 500) 
+                price *= 0.97;
+
+            System.Console.WriteLine($"Price with discount = {price}");
+        }
+
+        static double Read(string name) {
+            System.Console.Write($"{name} = ");
+            return double.Parse(Console.ReadLine());
         }
     }
 }
