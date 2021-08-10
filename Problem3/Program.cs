6df575c1671230a439dfdf4507c5716c6505116d
ShirChoi <abdulloevsarvar2005@gmail.com>
2021-08-10 22:31:34 +0500

    +problem3

---
+++ Problem3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Problem3 {
+    class Program {
+        static void Main(string[] args) {
+            int a = ReadInt("a");
+            int b = ReadInt("b");
+            int c = ReadInt("c");
+
+            if(b > a)
+                Swap(ref a, ref b);
+            
+            if(c > a)
+                Swap(ref a, ref c);
+
+            if(c > b)
+                Swap(ref b, ref c);
+
+            System.Console.WriteLine($"a = {a}, b = {b}, c = {c}");    
+        }
+
+        static void Swap(ref int a, ref int b) {
+            int c = a;
+            a = b;
+            b = c;
+        }
+
+        static int ReadInt(string name) {
+            System.Console.Write($"{name} = ");
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
